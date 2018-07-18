65b73c98a9a65183085d09c7c62a50330d067478
michal <michal.drajna@gmail.com>
2018-07-18 06:09:02 +0000

    Initial application

---
+++ mdblog/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+flask_app = Flask(__name__)
+
+@flask_app.route("/")
+def view_welcome_page():
+    return render_template("welcome.jinja")
+
+@flask_app.route("/about/")
+def view_about_page():
+    return render_template("about.jinja")
